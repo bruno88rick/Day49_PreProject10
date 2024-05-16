cdcff525a4c9fe98388f98e2c672750e3ffd945b
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-16 04:59:07 -0300

    Initial Commit

---
+++ Day49_PreProject10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day49_PreProject10
+//
+//  Created by Bruno Oliveira on 16/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day49_PreProject10/Day49_PreProject10App.swift
@@ -0,0 +1,17 @@
+//
+//  Day49_PreProject10App.swift
+//  Day49_PreProject10
+//
+//  Created by Bruno Oliveira on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Day49_PreProject10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
